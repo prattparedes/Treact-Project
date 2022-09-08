0c076f6f86761737e7f84339e1f7611830599189
Pratt <pratt.paredes@gmail.com>
2022-09-07 22:02:08 -0500

    pseudo-carrusel con algo de js y css

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function closeMenu() {
 
 }
 
+
 function leftClick() {
     document.body.classList += " view--testimonial-1"
     document.body.classList.remove('view--testimonial-2')
@@ -15,4 +16,17 @@ function leftClick() {
 function rightClick() {
     document.body.classList += " view--testimonial-2"
     document.body.classList.remove('view--testimonial-1')
+}
+
+var element = document.getElementsByClassName('view--testimonial-1');
+var element2 = document.getElementsByClassName('view--testimonial-2');
+
+function Clicks() {
+    if(element.length > 0){
+    rightClick();
+    } else if(element2.lenght > 0){
+    rightClick();
+    } else {
+    leftClick();
+    }
 }
